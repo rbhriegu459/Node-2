26e5a71eb9611e56a83685434192140704d90061
rbhriegu459 <rbhriegu459@gmail.com>
2023-12-08 13:57:25 +0530

    Array

--- main.js
+++ main.js
@@ -12,3 +12,12 @@ const Student = {
 
 Student.greetTeacher();
 
+const array = ['apple', 'oranges' , ' ', 'mango', ' ' , 'lemon'];
+
+console.log(array.map(item =>  {
+    return item === ' ' ? 'empty string' : item;
+}));
+console.log(array);
+
+// What according to you is spread opertor. Does spread operator create a new object or point to the same old object?
+// Spread operator is used to create a copy of another object. It creates a new object.
